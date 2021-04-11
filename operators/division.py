cdf618fa9d6562a1e66ee5b6f5796981ffec5399
Barr Kirel <barr.kirel.41@gmail.com>
2021-04-11 22:12:10 +0300

    Quick fix

--- operators/division.py
+++ operators/division.py
@@ -7,7 +7,7 @@ class Division(operator.Operator):
         The addition operator, this operator allows us to divide one number in the other
     """
     def __init__(self):
-        super().__init__('+')
+        super().__init__('/')
 
     def act(self, x: int, y: int) -> int:
         return x // y
